e714ea68594d7f7a68195b0138163d60ff285587
Diego Pinto <20265093@esen.edu.sv>
2026-09-17 15:12:09 -0600

    Commit 2: trabajando con variables

--- variables.py
+++ variables.py
@@ -1,3 +1,9 @@
 # esta sección será para aprender el uso de variable 
-print ("Hola mundo")
-print ("Hoy es mañana")
+print ("Sistema de mensajería")
+distancia_km = 6.5
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+
+distancia_km = distancia_km + 9.7
+print (distancia_km)
